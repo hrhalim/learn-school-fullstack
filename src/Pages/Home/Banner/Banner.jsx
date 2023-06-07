9e5981f637f65a1eb46ff43d4efb1cad12a97a2a
HR Halim <jshalim24@gmail.com>
2023-06-07 22:17:51 +0600

    Added Home banner

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,23 +1,56 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css";
+import "./Banner.css"
+// import required modules
+import { Autoplay, Navigation } from "swiper";
 
 const Banner = () => {
     return (
         <>
-             <Carousel>
-                <div>
-                    <img src="https://images.pexels.com/photos/7245482/pexels-photo-7245482.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" /> 
-                </div>
-                <div>
-                    <img src="https://images.pexels.com/photos/4210808/pexels-photo-4210808.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" /> 
-                </div>
-                <div>
-                    <img src="https://images.pexels.com/photos/7693952/pexels-photo-7693952.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" /> 
-                </div>
-                <div>
-                    <img src="https://images.pexels.com/photos/7693952/pexels-photo-7693952.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" /> 
-                </div>
-            </Carousel>
+            <div className="banner-slider relative">
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                loop={true}
+                autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+                }}
+                pagination={{
+                clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="banner-slide bg-[url('https://images.pexels.com/photos/8106191/pexels-photo-8106191.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]"> 
+                    <div className="banner-content py-28 md:py-64">
+                        <h2 className="text-white text-3xl md:text-4xl font-bold pb-5">Solfeggio Best Teachers</h2>
+                        <button className="btn btn-success">Book Online</button>
+                    </div> 
+                    </div>
+                </SwiperSlide> 
+                <SwiperSlide>
+                    <div className="banner-slide bg-[url('https://images.pexels.com/photos/4898305/pexels-photo-4898305.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]"> 
+                    <div className="banner-content py-28 md:py-64">
+                        <h2 className="text-white text-3xl md:text-4xl font-bold pb-5">solfeggio image Award-Winning Process</h2>
+                        <button className="btn btn-success">Book Online</button>
+                    </div> 
+                    </div>
+                </SwiperSlide> 
+                <SwiperSlide>
+                    <div className="banner-slide bg-[url('https://images.pexels.com/photos/8106193/pexels-photo-8106193.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]"> 
+                    <div className="banner-content py-28 md:py-64">
+                      <h2 className="text-white text-3xl md:text-4xl font-bold pb-5">Award-Winning Process</h2>
+                       <button className="btn btn-success">Book Online</button>
+                    </div> 
+                    </div>
+                </SwiperSlide>  
+            </Swiper>
+            </div>
         </>
     );
 };
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
-        <div>
-            HOME
-        </div>
+        <>
+            <Banner></Banner>
+        </>
     );
 };
 
